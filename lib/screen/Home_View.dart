056527766ee986bbdd88c096691e0c70aaaf1438
Carlos Castro <carloscastrogames@gmail.com>
2020-06-09 19:55:57 -0300

    vers 1.0

--- lib/screen/Home_View.dart
+++ lib/screen/Home_View.dart
@@ -5,17 +5,8 @@ class Home extends StatelessWidget {
   //teste
 
   final List<Transaction> _transaction = [
-    Transaction(
-      id: "t1",
-     title: "lala", 
-     value: 10.0,
-      date: DateTime.now()
-      ),
-    Transaction(
-      id: "t2", 
-      title: "carlos",
-       value: 9.99,
-        date: DateTime.now()),
+    Transaction(id: "t1", title: "lala", value: 10.0, date: DateTime.now()),
+    Transaction(id: "t2", title: "carlos", value: 9.99, date: DateTime.now()),
   ];
 
   @override
@@ -33,8 +24,38 @@ class Home extends StatelessWidget {
                 child: Text("Grafico"),
               ),
             ),
-            Card(
-              child: Text("Listas"),
+            Column(
+              children: <Widget>[
+                ..._transaction.map((e) {
+                  return Card(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 10,
+                          ),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                            color: Colors.black,
+                            width: 2,
+                          )),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            e.value.toString(),
+                          ),
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text(e.title),
+                            Text(e.date.toString()),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                })
+              ],
             ),
           ],
         ));
